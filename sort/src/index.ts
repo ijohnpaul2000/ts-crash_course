d850893d2bb5c10b931615c93998110822de7c93
John Paul Pineda <ijohnpaul2000@gmail.com>
2022-10-25 22:18:03 +0800

    80 extracting key logic

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -5,11 +5,21 @@ class Sorter {
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        // All of this only works if collection is number[]
+        // If collection is an array of numbers
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
         }
+
+        if (typeof this.collection === "string") {
+        }
+
+        // If collection is a string, do this logic instead:
+        // Logic to compare and swap characters in a string
       }
     }
   }
